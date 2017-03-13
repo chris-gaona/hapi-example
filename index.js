e3f47df79810909a9f83586a54f1b0693accdf23
Chris Gaona <chris.gaona@sbcglobal.net>
2017-03-13 16:36:08 -0700

    Initial commit creating Hapi server

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({
+    host: 'localhost',
+    port: 8000
+});
+
+function handler(request, reply) {
+    reply(request.params);
+}
+
+let goodOptions = {
+    reporters: {
+        myConsoleReporter: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            // can set log: ['error'] to only show error tagged log events
+            args: [{ log: '*', response: '*' }]
+        }, {
+            module: 'good-console'
+        }, 'stdout']
+    }
+};
+
+server.register({
+    register: require('good'),
+    options: goodOptions
+}, err => {
+    if (err) {
+        return console.error(err);
+    }
+
+    // use a ? to make the parameter optional
+    // use
+    server.route({
+        method: 'GET',
+        path: '/users/{userId?}',
+        handler: handler
+    });
+
+    server.start(() => console.log(`Started at: ${server.info.uri}`));
+});
